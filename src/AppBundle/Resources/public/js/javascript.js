79c92bdac32301003833a3f272e580f0a037b28f
rs <schmidt91robin@icloud.com>
2015-11-13 12:03:57 +0100

    fixed javascript bs

--- src/AppBundle/Resources/public/js/javascript.js
+++ src/AppBundle/Resources/public/js/javascript.js
@@ -1,15 +1,18 @@
 // JavaScript Document
 $(document).ready(function(){
+	/* Sollte in ein allgemeines JavaScriptfile verschoben werden, Anfang */
 	function addHeaderBG() {
-		if($(document).scrollTop() == 0) {
-    		$('header .navbar-fixed-top').removeClass('black');
-    	} else {
-    		$('header .navbar-fixed-top').addClass('black');
-    	}
-	}
-	addHeaderBG();
-        
-	$(document).scroll(function(){
-		addHeaderBG();
-    });
+	  if($(document).scrollTop() == 0) {
+		  $('body').addClass('white-color');
+		  $('body').removeClass('black-color');
+	   } else {
+		  $('body').removeClass('white-color');
+		  $('body').addClass('black-color');
+		}
+	 }
+	 addHeaderBG();
+
+	 $(document).scroll(function(){
+	   addHeaderBG();
+	  });
 });
