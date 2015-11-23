b6e899074c9d6e3a471064ce0458d700d72bce33
kasabina <office@sabinakargl.at>
2015-11-23 14:28:35 +0100

    Slider verbessert

--- src/AppBundle/Resources/public/js/javascript.js
+++ src/AppBundle/Resources/public/js/javascript.js
@@ -1,14 +1,16 @@
 // JavaScript Document
 $(document).ready(function(){
-	/* Sollte in ein allgemeines JavaScriptfile verschoben werden, Anfang */
+	/* Headerbackground */
+	var headerScrollheight = 50;
+	
 	function addHeaderBG() {
-	  if($(document).scrollTop() == 0) {
-		  $('body').addClass('head-white-color');
+	  if($(document).scrollTop() <= headerScrollheight) {
+	      $('body').addClass('head-white-color');
 		  $('body').removeClass('head-black-color');
 		  $('body').addClass('head-transparent-bg');
 		  $('body').removeClass('head-white-bg');
 		  $('.navbar-fixed-top').removeClass('dropshadow');
-	   } else {
+	   } else if ($(document).scrollTop() > headerScrollheight) {
 		  $('body').removeClass('head-white-color');
 		  $('body').addClass('head-black-color');
 		  $('body').addClass('head-white-bg');
@@ -19,6 +21,80 @@ $(document).ready(function(){
 	 addHeaderBG();
 
 	 $(document).scroll(function(){
-	   addHeaderBG();
-	  });
+	     addHeaderBG();
+	 });
+	  
+	 /*Slider*/
+	 
+	 /*Alle Bilder auf gleiche Höhe bringen (nach der kleinsten Höhe)*/
+	function carouselNormalization() {
+        var items = jQuery('#slider .item'), //grab all slides
+        width = [], //create empty array to store height values
+        //shortest; //create variable to make note of the shortest slide
+		Winheight,
+		Winwidth;
+
+        if (items.length) {
+			function Imagesize(el) {
+				aspectRatio = $(el).width() / $(el).height();
+				return aspectRatio;
+			}
+			
+            function normalizeHeights() {
+				var Winheight = $( window ).height();
+				var Winwidth = $( window ).width();
+				
+				if (width.length == 0) { //beim ersten Mal Array füllen
+                	items.each(function() { //Aspect Ratio Array hinzufügen
+						aspectRatio = Imagesize(this);
+                    	width.push(aspectRatio);
+                	});
+				}
+				
+				var i=0;
+                items.each(function() {
+					
+                    jQuery('.carousel-inner').css('height',Winheight + 'px').css('overflow','hidden');
+					
+					newWidth = Winheight * width[i];
+					if (newWidth >= Winwidth) {
+						newMargin = (Winwidth - newWidth) / 2;
+						jQuery('.carousel-inner .item img').css('height',Winheight + 'px').css('width',newWidth + 'px').css('margin-left',newMargin + "px");
+					} else {
+						newHeight = parseInt(Winwidth / width[i]);
+					    jQuery('.carousel-inner .item img').css('height',newHeight + 'px').css('width',Winwidth + 'px').css('margin-left','0px');
+					}
+					i++;
+                });
+            };
+            normalizeHeights();
+
+            jQuery(window).on('resize orientationchange', function () {
+                Winheight = 0, Winwidth = 0; //reset vars
+                normalizeHeights(); //run it again
+            });
+        }
+    }
+    carouselNormalization();
+	
+	
+	 
+	 
+	 function loadWinHeight() {
+	     var Winheight = $( window ).height();
+		 //$('#slider .item img').css('height',Winheight);
+		 //$('#slider .item img').css('width','auto'); 
+		 //$('#slider').height(Winheight);                     
+	 }
+	 //loadWinHeight();
+	 
+	 function resizeWinHeight() {
+		 //$('#slider .item img').css('height','auto');
+		 //$('#slider .item img').css('width','100%');
+	 }
+	 $(window).on("resize", function(){
+        //resizeWinHeight();
+		//console.log('bis heir');
+    });
+	 
 });
