3e2b12f34d21ba6bf59ba0f242ae93edf5342005
kasabina <office@sabinakargl.at>
2016-05-24 14:31:10 +0200

    Layout einbauen

--- src/AppBundle/Resources/public/js/javascript.js
+++ src/AppBundle/Resources/public/js/javascript.js
@@ -1,5 +1,16 @@
 // JavaScript Document
+    (function() {
+        var path = '//easy.myfonts.net/v2/js?sid=10259(font-family=Helvetica+Neue+55+Roman)&sid=10261(font-family=Helvetica+Neue+75+Bold)&sid=10263(font-family=Helvetica+Neue+45+Light)&sid=10265(font-family=Helvetica+Neue+65+Medium)&key=vrsfByw6hK',
+            protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+            trial = document.createElement('script');
+        trial.type = 'text/javascript';
+        trial.async = true;
+        trial.src = protocol + path;
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(trial);
+    })();
 $(document).ready(function(){
+	
 	/* Headerbackground */
 	var headerScrollheight = $(window).height()/2;
 	var headerHeight = 180;
