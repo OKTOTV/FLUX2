38130568ec913fcfb149e0e7ef64ac8bd5cc4412
rs <schmidt91robin@icloud.com>
2016-10-10 14:12:49 +0200

    fixed slider controller namespaces

--- src/AppBundle/Controller/Backend/SeriesController.php
+++ src/AppBundle/Controller/Backend/SeriesController.php
@@ -27,35 +27,4 @@ class SeriesController extends Controller
         $series = $this->get('oktolab_media')->getSeries($uniqID);
         return ['series' => $series];
     }
-
-
-    /**
-     * @Route("/series/{uniqID}/blog", name="oktothek_series_blog_post")
-     * @Method({"GET", "POST"})
-     * @Template
-     */
-    // public function blogSeriesAction(Request $request, Series $series)
-    // {
-    //     $post = new Post();
-    //     $form = $this->createForm(new PostType(), $post, ['action' => $this->generateUrl('oktothek_series_blog_post', ['uniqID' => $series->getUniqID()])]);
-    //     $form->add('submit', 'submit', ['label' => 'oktothek.post_create_button', 'attr' => ['class' => 'btn btn-primary']]);
-    //
-    //     if ($request->getMethod() == "POST") { //sends form
-    //         $form->handleRequest($request);
-    //         if ($form->isValid()) {
-    //             $em = $this->getDoctrine()->getManager();
-    //             $series->addPost($post);
-    //             $em->persist($post);
-    //             $em->persist($series);
-    //             $em->flush();
-    //             $this->get('oktothek_notification_service')->createNewPostNotifications($post);
-    //             $this->get('session')->getFlashBag()->add('success', 'oktothek.success_create_post');
-    //             return $this->redirect($this->generateUrl('oktothek_show_series', ['uniqID' => $series->getUniqID()]));
-    //         } else {
-    //             $this->get('session')->getFlashBag()->add('error', 'oktothek.error_create_post');
-    //         }
-    //     }
-    //
-    //     return ['form' => $form->createView()];
-    // }
 }
--- src/AppBundle/Controller/Backend/SlideController.php
+++ src/AppBundle/Controller/Backend/SlideController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Entity\Slide;
-use MediaBundle\Entity\Episode;
+use AppBundle\Entity\Episode;
 use AppBundle\Entity\Post;
 use AppBundle\Form\Backend\SlideType;
 
