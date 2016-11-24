d2c6b8aff1b7096bf95ad3e8b881da46fd4b4fe0
rs <schmidt91robin@icloud.com>
2016-11-24 15:04:52 +0100

    included search behavior in master now

--- src/AppBundle/Model/SearchService.php
+++ src/AppBundle/Model/SearchService.php
@@ -55,14 +55,11 @@ class SearchService
 
         $query = new \Elastica\Query\Match();
         $query->setFieldQuery('name', $searchphrase);
-        // $query->setFieldFuzziness('name', 0.7);
-        // $query->setFieldMinimumShouldMatch('name', '40%');
 
         $boolQuery->addShould($query);
 
         $desc_query = new \Elastica\Query\Match();
         $desc_query->setFieldQuery('description', $searchphrase);
-        // $desc_query->setFieldFuzziness('description', 0.7);
 
         $boolQuery->addShould($desc_query);
 
