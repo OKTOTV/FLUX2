cd2d9cc3aae1e81dad95164db81fb0dd8d65828c
rs <schmidt91robin@icloud.com>
2017-02-15 13:56:08 +0100

    removing comments if the episode gets deleted

---
+++ src/AppBundle/Event/DeleteEpisodeEventListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Event;
+
+use Oktolab\MediaBunle\Event\DeleteEpisodeEvent;
+
+class DeleteEpisodeEventListener {
+
+    private $em;
+
+    public function __construct($em)
+    {
+        $this->em = $em;
+    }
+
+    public function onEpisodeDelete(DeleteEpisodeEvent $event)
+    {
+        $comments = $this->em->getRepository('AppBundle:Comment')->findBy(['referer' => $event->getEpisode()->getUniqID()]);
+        foreach ($comments as $comment) {
+            $this->em->remove($comment);
+        }
+        $this->em->flush();
+    }
+}
+
+?>
