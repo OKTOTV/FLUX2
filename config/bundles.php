5d58f16948891f93ee054ac93ad9611da13a7f0b
rs <schmidt91robin@icloud.com>
2018-01-15 10:24:21 +0100

    updated configs, added profiler
    fixed some deprecations and double entries

--- config/bundles.php
+++ config/bundles.php
@@ -30,4 +30,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
