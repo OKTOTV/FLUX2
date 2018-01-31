6dcca5e95f3b9277e27becb9dfee36a46c5d4ad2
rs <schmidt91robin@icloud.com>
2018-01-31 09:44:16 +0100

    fixed missing include

--- src/AppBundle/Form/Backend/AttendeeType.php
+++ src/AppBundle/Form/Backend/AttendeeType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Intl\Intl;
 use Symfony\Component\Validator\Constraints as Assert;
